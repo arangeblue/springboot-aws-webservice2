2b9f4f740f4816c441dd0d83bd95994d6fbafa14
Kyle-Wi <black1848@naver.com>
2021-09-08 01:36:19 +0900

    20210908 "part 1 정리 시작"

---
+++ src/main/java/com/orangeblue/book2/springbootawswebservice2/SpringbootAwsWebservice2Application.java
@@ -0,0 +1,13 @@
+package com.orangeblue.book2.springbootawswebservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsWebservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsWebservice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/orangeblue/book2/springbootawswebservice2/SpringbootAwsWebservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangeblue.book2.springbootawswebservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsWebservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
